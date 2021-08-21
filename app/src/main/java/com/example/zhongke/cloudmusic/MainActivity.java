b7a016b6781bd3bd1bc158a80bb55bde45d99ba8
zk <zjtzlqr@163.com>
2021-08-21 16:55:47 +0800

    0821-dev-tijiao

--- app/src/main/java/com/example/zhongke/cloudmusic/MainActivity.java
+++ app/src/main/java/com/example/zhongke/cloudmusic/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zhongke.cloudmusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        Log.i("zk2021", "dev1-11111");
     }
 }

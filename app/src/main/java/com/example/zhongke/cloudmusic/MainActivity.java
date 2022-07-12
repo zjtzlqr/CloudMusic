31da1e067d6470c8ce21bd8c6e963f9fd21f3353
zjtzlqr <zjtzlqr@163.com>
2022-07-12 11:18:17 +0800

    111111zk

--- app/src/main/java/com/example/zhongke/cloudmusic/MainActivity.java
+++ app/src/main/java/com/example/zhongke/cloudmusic/MainActivity.java
@@ -6,16 +6,24 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String value = "3";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        Log.i("TAG", "bbb is sstart");
+        String b = null;
+        boolean bbb = value.equals(b);
+        Log.i("TAG", "bbb is " +bbb);
     }
 
     private void init(){
         Log.i("zk2021", "20220708");
         Log.i("zk2021", "20220708zkzkzk");
         Log.i("zk2021", "wyl20220708xiugai");
+
+        Log.i("zk2021", "zijixiugi");
     }
 }
